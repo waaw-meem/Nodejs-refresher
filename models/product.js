0e728c1b327fcf7794dbc41629dc276f683875df
Wali Muhammad <wm401238@gmail.com>
2023-07-07 09:04:00 +0500

    Work in Progress now we are enhancing

--- models/product.js
+++ models/product.js
@@ -8,44 +8,51 @@ const p = path.join(
   'products.json'
 );
 
+const getProductsFromFile = cb => {
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        reject(err);
+      } else {
+        let products = [];
+        try {
+          if (fileContent.length > 0) {
+              products = JSON.parse(fileContent)
+            }
+         
+        } catch (error) {
+          console.log('Error parsing JSON:', error);
+        }
+        resolve(cb(products));
+      }
+    });
+  }).catch(() => {
+    cb();
+  });
+}
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log("ok",err);
       });
-    });
+    })
+    // fs.readFile(p, (err, fileContent) => {
+      // let products = [];
+      // if (!err) {
+      //   products = JSON.parse(fileContent);
+      // }
+    
   }
 
   static fetchAll(cb) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(p, (err, fileContent) => {
-        if (err) {
-          reject(err);
-        } else {
-          let products = [];
-          try {
-            if (fileContent.length > 0) {
-                products = JSON.parse(fileContent)
-              }
-           
-          } catch (error) {
-            console.log('Error parsing JSON:', error);
-          }
-          resolve(cb(products));
-        }
-      });
-    }).catch(() => {
-      cb();
-    });
+    getProductsFromFile(cb)
   }
 };
